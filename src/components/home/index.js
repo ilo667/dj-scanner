c8e73d6161dcae28765d85516d25c48f65196d3f
ilo667 <ilo666777@gmail.com>
2026-02-26 11:03:48 +0200

    Update homepage

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -6,7 +6,7 @@ export default function Home() {
             <h1 className="mb-4 text-3xl font-bold tracking-tight">DJ Scanner - ваш помічник у редагуванні плейлистів</h1>
             <p>Що на данний час вміє DJ Scanner:</p>
             <ul className="list-disc list-inside mb-5">
-                <li>Читання треклістів із <strong>CUE-файлів</strong></li>
+                <li>Читання треклістів із <strong>CUE-файлів та TXT-експорту з Beatport</strong></li>
                 <li>Ручне введення треклістів через <strong>поле</strong></li>
                 <li>Розпізнавання тексту зі <strong>скріншотів і зображень</strong></li>
                 <li>Автоматичне визначення <strong>артистів</strong></li>
