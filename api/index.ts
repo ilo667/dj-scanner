16f738d61f5f2f5dbb34e5875b0339d3eba77d86
ilo667 <ilo666777@gmail.com>
2026-05-19 12:21:39 +0300

    Remove unused code

--- api/index.ts
+++ api/index.ts
@@ -4,15 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const scanRoutes = require('./routes/scan');
 const artistsRoutes = require('./routes/artists');
-const ocrRoutes = require('./routes/ocr');
-
 const app = express();
 
 app.set('port', (process.env.PORT || 8081));
 app.use(bodyParser.json());
 app.use('/api/scan', scanRoutes);
 app.use('/api/artists', artistsRoutes);
-app.use('/api/ocr', ocrRoutes);
 
 if (process.env.NODE_ENV !== 'production') {
 	const port = process.env.PORT || 8081;
