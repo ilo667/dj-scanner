6ecfb7576a153c741b47698be206c1121723c715
ilo667 <ilo666777@gmail.com>
2026-05-29 21:11:37 +0300

    Code refactor

--- api/index.ts
+++ api/index.ts
@@ -9,6 +9,7 @@ if (!process.env.POSTGRES_URL) {
 }
 
 const express = require('express');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const scanRoutes = require('./routes/scan');
 const artistsRoutes = require('./routes/artists');
@@ -18,6 +19,7 @@ const genresRoutes = require('./routes/genres');
 const app = express();
 
 app.set('port', (process.env.PORT || 8081));
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/scan', scanRoutes);
